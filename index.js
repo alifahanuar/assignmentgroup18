0f83c682b8ac7893010a46b1be28e9e6a6d5c334
alifahanuar <alfhanuar@gmail.com>
2023-12-27 09:24:38 +0800

    update

--- index.js
+++ index.js
@@ -52,13 +52,8 @@ const options = {
   // Dummy user data (replace with a proper authentication system)
 const admins = [
     { username: 'user1', password: 'password1' },
-    { username: 'user2', password: 'password2' },
   ];
   
-  // Routes
-app.get('/', (req, res) => {
-    res.render('login');
-  });
   
 
   
@@ -93,46 +88,6 @@ const adminsCollection = db.collection('admins');
 const visitorPassCollection = db.collection('visitorpass');
 
 function login(reqUsername, reqPassword) { 
-    return usersCollection.findOne({ username: reqUsername, password: reqPassword }) 
-      .then(matchUsers => { 
-        if (!matchUsers) { 
-          return { 
-            success: false, 
-            message: "User not found!" 
-          }; 
-        } else { 
-          return { 
-            success: true, 
-            users: matchUsers
-          }; 
-        } 
-      }) 
-      .catch(error => { 
-        console.error('Error in login:', error); 
-        return { 
-          success: false, 
-          message: "An error occurred during login." 
-        }; 
-      }); 
-  } 
-    
-  
-  function register(reqUsername, reqPassword, reqName, reqEmail) { 
-    return usersCollection.insertOne({ 
-      username: reqUsername, 
-      password: reqPassword, 
-      name: reqName, 
-      email: reqEmail 
-    }) 
-      .then(() => { 
-        return "Registration Admin Successful!"; 
-      }) 
-      .catch(error => {      console.error('Error in register:', error); 
-        return "An error occurred during registration."; 
-      }); 
-  } 
-
-function loginAdmins(reqUsername, reqPassword) { 
   return adminsCollection.findOne({ username: reqUsername, password: reqPassword }) 
     .then(matchAdmins => { 
       if (!matchAdmins) { 
@@ -157,7 +112,7 @@ function loginAdmins(reqUsername, reqPassword) {
 } 
   
 
-function registerAdmins(reqUsername, reqPassword, reqName, reqEmail) { 
+function register(reqUsername, reqPassword, reqName, reqEmail) { 
   return adminsCollection.insertOne({ 
     username: reqUsername, 
     password: reqPassword, 
@@ -171,6 +126,45 @@ function registerAdmins(reqUsername, reqPassword, reqName, reqEmail) {
       return "An error occurred during registration."; 
     }); 
 } 
+// function login(reqUsername, reqPassword) { 
+//   return adminsCollection.findOne({ username: reqUsername, password: reqPassword }) 
+//     .then(matchAdmins => { 
+//       if (!matchAdmins) { 
+//         return { 
+//           success: false, 
+//           message: "Admin not found!" 
+//         }; 
+//       } else { 
+//         return { 
+//           success: true, 
+//           users: matchAdmins
+//         }; 
+//       } 
+//     }) 
+//     .catch(error => { 
+//       console.error('Error in login:', error); 
+//       return { 
+//         success: false, 
+//         message: "An error occurred during login." 
+//       }; 
+//     }); 
+// } 
+  
+
+// function register(reqUsername, reqPassword, reqName, reqEmail) { 
+//   return adminsCollection.insertOne({ 
+//     username: reqUsername, 
+//     password: reqPassword, 
+//     name: reqName, 
+//     email: reqEmail 
+//   }) 
+//     .then(() => { 
+//       return "Registration Admin Successful!"; 
+//     }) 
+//     .catch(error => {      console.error('Error in register:', error); 
+//       return "An error occurred during registration."; 
+//     }); 
+// } 
 
 
 
@@ -207,20 +201,6 @@ app.post('/register', (req, res) => {
   }); 
 }); 
 
-// Register route 
-app.post('/registeradmin', (req, res) => { 
-    console.log(req.body); 
-   
-    let result = registerAdmins(req.body.username, req.body.password, req.body.name, req.body.email); 
-    result.then(response => { 
-      res.send(response); 
-    }).catch(error => { 
-      console.error('Error in register route:', error); 
-      res.status(500).send("An error occurred during registration."); 
-    }); 
-  }); 
-
-
 /**
  * @swagger
  * /login:
@@ -229,31 +209,38 @@ app.post('/registeradmin', (req, res) => {
  *     description: User Authentication.
  *     tags:
  *       - Authentication
- *     requestBody:
+ *     requestbody:
  *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - username
- *               - password
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       '200':
- *         description: Successful login
- *         content:
- *           application/json:
- *             example:
- *               token: "your_access_token"
- *       '401':
- *         description: Unauthorized
- *       '500':
- *         description: "An error occurred during login"
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                username:
+                  type: string
+                  description: User's username
+                password:
+                  type: string
+                  description: User's password
+      responses:
+        '200':
+          description: Successful login
+          content:
+            application/json:
+              example:
+                token: "your_access_token"
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              example:
+                error: "Invalid credentials"
+        '500':
+          description: Internal Server Error
+          content:
+            application/json:
+              example:
+                error: "An error occurred during login"
  */
 
 // Login route 
@@ -273,37 +260,6 @@ app.post('/login', (req, res) => {
     res.status(500).json({ error: 'An error occurred during login', details: error.message });
   }); 
 }); 
-
-/**
- * @swagger
- * /loginadmin:
- *   post:
- *     summary: Admin
- *     description: Login Admin Page
- *     tags:
- *       - Authentication
- *     responses:
- *       201:
- *         description: Provide Message
- *        content:
- *           application/json:
- *             example:
- *               message: "Copy and navigate to this link "
- */
-
-
-app.post('/loginadmin', (req, res) => {
-    const { username, password } = req.body;
-  
-    // Dummy authentication (replace with a proper authentication system)
-    const adminsCollection = admins.find((admin) => admin.username === username && admin.password === password);
-  
-    if (user) {
-      res.send(`Welcome, ${username}!`);
-    } else {
-      res.send('Invalid username or password.');
-    }
-  });
 
 /**
  * @swagger
