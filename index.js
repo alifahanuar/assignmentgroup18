890e446fc0f6826ff83df6c5a5199bc3ce5e81cb
alifahanuar <alfhanuar@gmail.com>
2024-01-16 17:25:09 +0800

    update

--- index.js
+++ index.js
@@ -267,15 +267,22 @@ app.post('/registeradmin', (req, res) => {
  *     description: User Authentication.
  *     tags:
  *       - Login
- *     parameters:
- *       - name: username
- *         in: query
- *         required: true
- *         type: string
- *       - name: password
- *         in: query
- *         required: true
- *         type: string
+ *     requestBody:
+ *       description: Create a visitor
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string 
+ *               password:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
  *     responses:
  *       '200':
  *         description: Successful login
